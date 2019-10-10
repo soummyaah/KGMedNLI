ef8912bae638ce0bd6f5999b9c3312eb6da32f71
soummyaah <soumya13106@iiitd.ac.in>
2019-10-10 18:55:49 +0530

    Changed layers file to reflect weighted attention and return similarity matrix to check valuues

--- esim/layers.py
+++ esim/layers.py
@@ -4,6 +4,8 @@ Definition of custom layers for the ESIM model.
 # Aurelien Coet, 2018.
 
 import torch.nn as nn
+import torch
+from torch.nn import Parameter
 
 from .utils import sort_by_seq_lens, masked_softmax, weighted_sum
 
@@ -97,7 +99,7 @@ class Seq2SeqEncoder(nn.Module):
                                  dropout=dropout,
                                  bidirectional=bidirectional)
 
-    def forward(self, sequences_batch, sequences_lengths):
+    def forward(self, sequences_batch, sequences_lengths, total_length):
         """
         Args:
             sequences_batch: A batch of variable length sequences of vectors.
@@ -119,7 +121,7 @@ class Seq2SeqEncoder(nn.Module):
         outputs, _ = self._encoder(packed_batch, None)
 
         outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs,
-                                                      batch_first=True)
+                                                      batch_first=True, total_length=total_length)
         reordered_outputs = outputs.index_select(0, restoration_idx)
 
         return reordered_outputs
@@ -161,11 +163,78 @@ class SoftmaxAttention(nn.Module):
                 premises in the input batch.
             attended_hypotheses: The sequences of attention vectors for the
                 hypotheses in the input batch.
+            similarity_matrix: The dot product between premises and hypothesis 
+                in the input batch.
         """
         # Dot product between premises and hypotheses in each sequence of
         # the batch.
         similarity_matrix = premise_batch.bmm(hypothesis_batch.transpose(2, 1)
                                                               .contiguous())
+
+        # Softmax attention weights.
+        prem_hyp_attn = masked_softmax(similarity_matrix, hypothesis_mask)
+        hyp_prem_attn = masked_softmax(similarity_matrix.transpose(1, 2)
+                                                        .contiguous(),
+                                       premise_mask)
+
+        # Weighted sums of the hypotheses for the the premises attention,
+        # and vice-versa for the attention of the hypotheses.
+        attended_premises = weighted_sum(hypothesis_batch,
+                                         prem_hyp_attn,
+                                         premise_mask)
+        attended_hypotheses = weighted_sum(premise_batch,
+                                           hyp_prem_attn,
+                                           hypothesis_mask)
+
+        return attended_premises, attended_hypotheses, similarity_matrix
+
+class WeightedAttention(nn.Module):
+    """
+    Attention layer taking premises and hypotheses encoded by an RNN as input
+    and computing the soft attention between their elements.
+
+    The dot product of the encoded vectors in the premises and hypotheses is
+    first computed. The softmax of the result is then used in a weighted sum
+    of the vectors of the premises for each element of the hypotheses, and
+    conversely for the elements of the premises.
+    """
+
+    def __init__(self, embedding_dim):
+        super(WeightedAttention, self).__init__()
+        self.w = Parameter(torch.Tensor(embedding_dim, embedding_dim))
+        torch.nn.init.xavier_normal(self.w)
+
+    def forward(self,
+                premise_batch,
+                premise_mask,
+                hypothesis_batch,
+                hypothesis_mask):
+        """
+        Args:
+            premise_batch: A batch of sequences of vectors representing the
+                premises in some NLI task. The batch is assumed to have the
+                size (batch, sequences, vector_dim).
+            premise_mask: A mask for the sequences in the premise batch, to
+                ignore padding data in the sequences during the computation of
+                the attention.
+            hypothesis_batch: A batch of sequences of vectors representing the
+                hypotheses in some NLI task. The batch is assumed to have the
+                size (batch, sequences, vector_dim).
+            hypothesis_mask: A mask for the sequences in the hypotheses batch,
+                to ignore padding data in the sequences during the computation
+                of the attention.
+
+        Returns:
+            attended_premises: The sequences of attention vectors for the
+                premises in the input batch.
+            attended_hypotheses: The sequences of attention vectors for the
+                hypotheses in the input batch.
+        """
+        # Dot product between premises and hypotheses in each sequence of
+        # the batch.
+        similarity_matrix = premise_batch.matmul(self.w.matmul(hypothesis_batch.transpose(2, 1)
+                                                              .contiguous()))
+
 
         # Softmax attention weights.
         prem_hyp_attn = masked_softmax(similarity_matrix, hypothesis_mask)
